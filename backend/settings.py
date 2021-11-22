c5dcd2c585d410d658756a8d5e427a96ddac676f
Aniket392 <ani24866@gmail.com>
2021-11-22 21:55:14 +0530

    Added Host

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-109-216-167.ap-south-1.compute.amazonaws.com:8000/']
 
 
 # Application definition
