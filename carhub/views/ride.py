1310d006c2a9cc00973d1d05ba5da295284a298c
Aniket392 <ani24866@gmail.com>
2021-12-08 12:31:44 +0530

    Fixed Bug RideCar

--- carhub/views/ride.py
+++ carhub/views/ride.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from carhub.models import Car, Order, City
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -16,14 +17,14 @@ def RideCar(request, city=None):
             toDate = timezone.now()+timedelta(days=1)
         if city is not None:
             car_all = Order.objects.filter(car__city__id = city)
-            car_notbooked = Car.objects.filter(city__id = city).exclude(car_detail__in=car_all).values('id', 'brand', 'modelName', 'year', 'category__name', 'details__price_by_model', 'user_id', 'city_id', 'photo', 'rc')
-            car_notbooked_date = Car.objects.filter(city__id = city,  car_detail__orderDateFrom__lt=fromDate, car_detail__orderDateExpire__gt=toDate).values('id', 'brand', 'modelName', 'year', 'category__name', 'details__price_by_model', 'user_id', 'city_id', 'photo', 'rc')
-            car_data = list(car_notbooked | car_notbooked_date)
+            car_notbooked = Car.objects.filter(city__id = city).exclude(car_detail__in=car_all).values('id', 'brand', 'modelName', 'year', 'category__name', 'details__price_by_model', 'user_id', 'city_id', 'photo')
+            car_notbooked_date = Car.objects.filter(Q(city_id=city) & (Q(car_detail__orderDateFrom__gt=toDate) | Q(car_detail__orderDateExpire__lt=fromDate))).distinct().values('id', 'brand', 'modelName', 'year', 'category__name', 'details__price_by_model', 'user_id', 'city_id', 'photo')
+            car_data = list(car_notbooked.union(car_notbooked_date))
 
             # For Taking out URL from object
             for car in car_data:
                 car['photo'] = car['photo'].url
-                car['rc'] = car['rc'].url
+                # car['rc'] = car['rc'].url
 
             return JsonResponse({'data': car_data})
         else:
