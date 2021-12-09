1ea59e53400a12f66db530efbc461a276be9f882
Aniket392 <ani24866@gmail.com>
2021-12-09 12:22:03 +0530

    Added Category Filter for Riding

--- carhub/views/ride.py
+++ carhub/views/ride.py
@@ -11,6 +11,9 @@ def RideCar(request, city=None):
     if request.user.is_authenticated:
         fromDate = request.GET.get('fromDate', '')
         toDate = request.GET.get('toDate', '')
+        category = request.GET.get('category', [])
+
+
         if fromDate == '':
             fromDate = timezone.now()+timedelta(days=1)
         if toDate == '':
@@ -20,12 +23,16 @@ def RideCar(request, city=None):
 
             car_notbooked = Car.objects.filter(city__id = city).exclude(car_detail__in=car_all).annotate(no_of_order=Count('car_detail__id', filter = Q(car_detail__status = 'COM'))).values('id', 'brand', 'modelName', 'year', 'category__name', 'details__price_by_model', 'user_id', 'city_id', 'photo', 'no_of_order')
             car_notbooked_date = Car.objects.filter(Q(city_id=city) & (Q(car_detail__orderDateFrom__gt=toDate) | Q(car_detail__orderDateExpire__lt=fromDate))).distinct().annotate(no_of_order=Count('car_detail__id', filter = Q(car_detail__status = 'COM'))).values('id', 'brand', 'modelName', 'year', 'category__name', 'details__price_by_model', 'user_id', 'city_id', 'photo', 'no_of_order')
+        
+            for cat in category:
+                car_notbooked = car_notbooked.filter(category = cat)        # Category filter
+                car_notbooked_date = car_notbooked_date.filter(category=cat)
+            
             car_data = list(car_notbooked.union(car_notbooked_date))
 
             # For Taking out URL from object
             for car in car_data:
                 car['photo'] = car['photo'].url
-                # car['rc'] = car['rc'].url
 
             return JsonResponse({'data': car_data})
         else:
