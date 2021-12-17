9f96f8857691c258992d3e46a6d0640c45684882
Aniket392 <ani24866@gmail.com>
2021-12-17 09:28:00 +0530

    Changed URL

--- carhub/urls.py
+++ carhub/urls.py
@@ -3,30 +3,31 @@ from django.urls.conf import include, re_path
 from carhub import views
 
 urlpatterns = [
-    path('', views.Home, name = ""),
+    path('', views.Index, name = "index"),
+    path('api/home', views.Home, name = "home"),
 
-    path('user/<int:pk>', views.UserDashboard, name="user-dashboard"),
-    path('user/<int:pk>/car-data', views.CarDataAPI, name="car-data"),
-    path('user/<int:pk>/bank-detail', views.BankDetails, name="bank-detail"),
-    path('user/<int:pk>/rider-order-detail', views.RiderOrderDetails, name="rider-order-detail"),
-    path('user/<int:pk>/report-now', views.ReportNow, name="report-now"),
+    path('api/user/<int:pk>', views.UserDashboard, name="user-dashboard"),
+    path('api/user/<int:pk>/car-data', views.CarDataAPI, name="car-data"),
+    path('api/user/<int:pk>/bank-detail', views.BankDetails, name="bank-detail"),
+    path('api/user/<int:pk>/rider-order-detail', views.RiderOrderDetails, name="rider-order-detail"),
+    path('api/user/<int:pk>/report-now', views.ReportNow, name="report-now"),
 
-    path('media/<str:folder>/<str:file>', views.protected_media, name="protected_media"),
+    path('api/media/<str:folder>/<str:file>', views.protected_media, name="protected_media"),
 
-    path('signup', views.Signup, name = "signup"),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
-    path('signin', views.Signin, name = "signin"),
-    path('signout', views.Signout, name = "signout"),
-
-
-    path('rentcar', views.RentCar, name = "rentcar"),
-
-    path('ridecar', views.RideCar, name = "ridecar"),
-    path('price-calculator', views.PriceCalculator, name = "pricecalculator"),
-    path('ridecar/<int:city>', views.RideCar, name = "ridecar"),
-    path('book/<int:carid>', views.Book, name = "bookcar"),
-    path('order-status/',views.handlerequest, name="order-status/"),
+    path('api/signup', views.Signup, name = "signup"),
+    path('api/activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('api/signin', views.Signin, name = "signin"),
+    path('api/signout', views.Signout, name = "signout"),
 
 
-    path('test',views.test)
+    path('api/rentcar', views.RentCar, name = "rentcar"),
+
+    path('api/ridecar', views.RideCar, name = "ridecar"),
+    path('api/price-calculator', views.PriceCalculator, name = "pricecalculator"),
+    path('api/ridecar/<int:city>', views.RideCar, name = "ridecar"),
+    path('api/book/<int:carid>', views.Book, name = "bookcar"),
+    path('api/order-status/',views.handlerequest, name="order-status/"),
+
+
+    path('api/test',views.test)
 ]
