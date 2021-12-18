814d1b5ecce215546a6b1005ae39ec6d524b71ea
Aniket392 <ani24866@gmail.com>
2021-12-18 11:38:44 +0530

    Fixed admin URL bug

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('carhub.urls')),
 ]
 if settings.DEBUG:
